5729f395b0ef9c3c2117cc8a149e21a56aef3ff7
michal-szostak <szostak.m.f@gmail.com>
2015-08-06 13:46:52 +0200

    added download_url to setup.py

--- setup.py
+++ setup.py
@@ -17,5 +17,6 @@ setup(
     license='',
     author='Michał Szostak',
     author_email='michal.florian.szostak@cern.ch',
-    description='Library providing means of conversion between oldhepdata format to new one, and new one to csv / yoda / root etc.'
+    description='Library providing means of conversion between oldhepdata format to new one, and new one to csv / yoda / root etc.',
+    download_url='https://github.com/HEPData/hepdata-converter/tarball/0.1',
 )
