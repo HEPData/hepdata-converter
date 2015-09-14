436443d628df627161207d3e191d78b9e4c22f6b
michal-szostak <szostak.m.f@gmail.com>
2015-09-14 14:40:24 +0200

    fixed tests for oldhepdata converter

--- hepdata_converter/testsuite/test_oldhepdata.py
+++ hepdata_converter/testsuite/test_oldhepdata.py
@@ -4,7 +4,10 @@ import StringIO
 import time
 import os
 import shutil
+import yaml
+from hepdata_converter import parsers
 import hepdata_converter
+from hepdata_converter.parsers import yaml_parser
 from hepdata_converter.parsers.oldhepdata_parser import OldHEPData
 from hepdata_converter.testsuite import insert_data_as_file, insert_path
 
@@ -22,43 +25,15 @@ class OldHEPDataTestSuite(unittest.TestCase):
         os.mkdir(self.tmp_dir_name)
 
     @insert_data_as_file('oldhepdata/sample.input')
-    def test_parse_submission(self, oldhepdata_file):
-        parser = OldHEPData()
-        parsed_data = parser.parse(oldhepdata_file)
+    @insert_path('oldhepdata/yaml')
+    def test_parse_submission(self, oldhepdata_file, yaml_path):
+        oldhepdata_p = OldHEPData()
+        oldhepdata_parsed_data = oldhepdata_p.parse(oldhepdata_file)
 
-        # test parsing of independent_variables
-        self.assertEqual(parsed_data.tables[0].data['independent_variables'][0], {'values': ['7000'], 'header': {'name': 'SQRT(S)', 'units': 'GEV'}})
+        yaml_p = yaml_parser.YAML()
+        yaml_parsed_data = yaml_p.parse(yaml_path)
 
-        # test parsing of dependent_variables
-        self.assertEqual(parsed_data.tables[0].data['dependent_variables'], [
-            {'header': {'units': 'FB', 'name': 'SIG(fiducial)'}, 'qualifiers': [{'name': 'RE', 'value': 'P P --> Z0 < LEPTON+ LEPTON- > Z0 < LEPTON+ LEPTON- > X'}], 'values': [
-                {'value': '25.4',
-                 'errors': [
-                    {'asymerror': {'plus': '3.3', 'minus': '-3.0'}, 'label': 'stat'},
-                    {'asymerror': {'minus': '-1.0', 'plus': '1.2'}, 'label': 'sys'},
-                    {'symerror': '1.0', 'label': 'sys,lumi'},
-                 ]
-                }
-            ]},
-            {'header': {'units': 'FB', 'name': 'SIG(fiducial)'}, 'qualifiers': [{'name': 'RE', 'value': 'P P --> Z0 < LEPTON+ LEPTON- > Z0* < LEPTON+ LEPTON- > X'}], 'values': [
-                {
-                    'value': '29.8',
-                    'errors': [{'asymerror': {'plus': '3.8', 'minus': '-3.5'}, 'label': 'stat'},
-                               {'asymerror': {'plus': '1.7', 'minus': '-1.5'}, 'label': 'sys'},
-                               {'symerror': '1.2', 'label': 'sys,lumi'}]
-                }
-            ]},
-            {'header': {'units': 'FB', 'name': 'SIG(fiducial)'}, 'qualifiers': [{'name': 'RE', 'value': 'P P --> Z0 < LEPTON+ LEPTON- > Z0 < NU NUBAR > X'}], 'values': [
-                {
-                    'value': '12.7',
-                    'errors': [
-                        {'asymerror': {'plus': '3.1', 'minus': '-2.9'}, 'label': 'stat'},
-                        {'symerror': '1.7', 'label': 'sys'},
-                        {'symerror': '0.5', 'label': 'sys,lumi'}
-                    ]
-                }
-            ]
-            }])
+        self.assertEqual(yaml_parsed_data, oldhepdata_parsed_data)
 
     @insert_path('oldhepdata/sample.input')
     def test_cli(self, oldhepdata_path):
